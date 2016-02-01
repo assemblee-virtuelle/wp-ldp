19dda4034a346f2afd0bcf46a65fd5b49dcfc587
balessan <benoit.alessandroni@gmail.com>
2016-02-01 19:11:31 +0100

    Add support of proper array generation in single resources template

--- single-ldp_resource.php
+++ single-ldp_resource.php
@@ -17,16 +17,64 @@
             $modelsDecoded = json_decode($termMeta["ldp_model"]);
             $fields = $modelsDecoded->{$value->slug}->fields;
 
+            $arrayToProcess = [];
+            $fieldNotToRender = [];
+            // Construct proper values array, if any, based on field endings with number:
             foreach($fields as $field) {
-              if(substr($field->name, 0, 4) == "ldp_") {
-                  echo('"'.substr($field->name, 4).'": ');
-                  echo('' . json_encode(get_post_custom_values($field->name)[0]) . ',');
-                  echo "\n        ";
+              $endsWithNumber = preg_match_all("/(.*)?(\d+)$/", $field->name, $matches);
+              if (!empty($matches)) {
+                if ($endsWithNumber > 0) {
+                  $fieldName = $matches[1][0];
+                  if (!in_array($fieldName, $arrayToProcess)) {
+                    $arrayToProcess[] = $fieldName;
+                  }
+
+                  // Generate proper array to exclude those fields from general rendering
+                  $excludedField = $matches[0][0];
+                  if (!in_array($excludedField, $fieldNotToRender)) {
+                    $fieldNotToRender[] = $excludedField;
+                  }
+
+                  if (!in_array($fieldName, $fieldNotToRender)) {
+                    $fieldNotToRender[] = $fieldName;
+                  }
+                }
               }
             }
-          ?>
+            // Example of arrayToProcess ['ldp_foaf:knows', 'ldp_foaf:currentProject']
 
-        "@id": "<?php the_permalink(); ?>"
+            foreach($arrayToProcess as $arrayField) {
+              foreach($fields as $field) {
+                if ( strstr($field->name, $arrayField) ||
+                    $field->name === $arrayField ) {
+                  $valuesArray[$arrayField][] = json_encode(get_post_custom_values($field->name)[0]);
+                }
+              }
+            }
+
+            foreach($fields as $field) {
+              if (substr($field->name, 0, 4) == "ldp_") {
+                if (!in_array($field->name, $fieldNotToRender)) {
+                  echo('          "'.substr($field->name, 4).'": ');
+                  echo('' . json_encode(get_post_custom_values($field->name)[0]) . ',');
+                  echo "\n";
+                }
+              }
+            }
+
+            foreach ($valuesArray as $fieldName => $values) {
+              echo("          \"" . substr($fieldName, 4) . "\": [\n");
+              foreach($values as $value) {
+                if (!empty($value) && $value != '""') {
+                  echo "               {\n";
+                  echo("                    \"@id\": " . $value . "\n");
+                  echo "               },\n";
+                }
+              }
+              echo "          ],\n";
+            }
+          ?>
+          "@id": "<?php the_permalink(); ?>"
         }
 <?php endwhile; ?>
     ]
