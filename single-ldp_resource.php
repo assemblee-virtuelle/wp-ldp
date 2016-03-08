73ed9b2788269c3297599f27a15aa338db42f91a
balessan <benoit.alessandroni@gmail.com>
2016-03-08 14:17:16 +0100

    Add workaround for managing array typed object properties

--- single-ldp_resource.php
+++ single-ldp_resource.php
@@ -17,72 +17,85 @@
             $modelsDecoded = json_decode($termMeta["ldp_model"]);
             $fields = $modelsDecoded->{$value->slug}->fields;
 
-            $arrayToProcess = [];
-            $fieldNotToRender = [];
-            // Construct proper values array, if any, based on field endings with number:
-            foreach($fields as $field) {
-              $endsWithNumber = preg_match_all("/(.*)?(\d+)$/", $field->name, $matches);
-              if (!empty($matches)) {
-                if ($endsWithNumber > 0) {
-                  $fieldName = $matches[1][0];
-                  if (!in_array($fieldName, $arrayToProcess)) {
-                    $arrayToProcess[] = $fieldName;
-                  }
+            $referer = $_SERVER['HTTP_REFERER'];
 
-                  // Generate proper array to exclude those fields from general rendering
-                  $excludedField = $matches[0][0];
-                  if (!in_array($excludedField, $fieldNotToRender)) {
-                    $fieldNotToRender[] = $excludedField;
-                  }
-
-                  if (!in_array($fieldName, $fieldNotToRender)) {
-                    $fieldNotToRender[] = $fieldName;
-                  }
-                }
-              }
-            }
-            // Example of arrayToProcess ['ldp_foaf:knows', 'ldp_foaf:currentProject']
-
-            foreach($arrayToProcess as $arrayField) {
+            // Handling special case of editing trhough the wordpress admin backend
+            if (strstr($referer, 'wp-admin/post.php')) {
               foreach($fields as $field) {
-                if ( strstr($field->name, $arrayField) ||
-                    $field->name === $arrayField ) {
-                  $value = get_post_custom_values($field->name)[0];
-                  if (!empty($value) && $value != '""') {
-                    $valuesArray[$arrayField][] = json_encode(get_post_custom_values($field->name)[0]);
-                  }
-                }
-              }
-            }
-
-            foreach($fields as $field) {
-              if (substr($field->name, 0, 4) == "ldp_") {
-                if (!in_array($field->name, $fieldNotToRender)) {
+                if (substr($field->name, 0, 4) == "ldp_") {
                   echo('          "'.substr($field->name, 4).'": ');
                   echo('' . json_encode(get_post_custom_values($field->name)[0]) . ',');
                   echo "\n";
                 }
               }
-            }
+            } else {
+              $arrayToProcess = [];
+              $fieldNotToRender = [];
+              // Construct proper values array, if any, based on field endings with number:
+              foreach($fields as $field) {
+                $endsWithNumber = preg_match_all("/(.*)?(\d+)$/", $field->name, $matches);
+                if (!empty($matches)) {
+                  if ($endsWithNumber > 0) {
+                    $fieldName = $matches[1][0];
+                    if (!in_array($fieldName, $arrayToProcess)) {
+                      $arrayToProcess[] = $fieldName;
+                    }
 
-            if (!empty($valuesArray)) {
-              foreach ($valuesArray as $fieldName => $values) {
-                echo("          \"" . substr($fieldName, 4) . "\": [\n");
-                $count = 0;
-                foreach($values as $value) {
-                  if (!empty($value) && $value != '""') {
-                    $count++;
-                    echo "               {\n";
-                    echo("                    \"@id\": " . $value . "\n");
+                    // Generate proper array to exclude those fields from general rendering
+                    $excludedField = $matches[0][0];
+                    if (!in_array($excludedField, $fieldNotToRender)) {
+                      $fieldNotToRender[] = $excludedField;
+                    }
 
-                    if ($count < count($values)) {
-                      echo "               },\n";
-                    } else {
-                      echo "               }\n";
+                    if (!in_array($fieldName, $fieldNotToRender)) {
+                      $fieldNotToRender[] = $fieldName;
                     }
                   }
                 }
-                echo "          ],\n";
+              }
+              // Example of arrayToProcess ['ldp_foaf:knows', 'ldp_foaf:currentProject']
+
+              foreach($arrayToProcess as $arrayField) {
+                foreach($fields as $field) {
+                  if ( strstr($field->name, $arrayField) ||
+                      $field->name === $arrayField ) {
+                    $value = get_post_custom_values($field->name)[0];
+                    if (!empty($value) && $value != '""') {
+                      $valuesArray[$arrayField][] = json_encode(get_post_custom_values($field->name)[0]);
+                    }
+                  }
+                }
+              }
+
+              if (!empty($valuesArray)) {
+                foreach ($valuesArray as $fieldName => $values) {
+                  echo("          \"" . substr($fieldName, 4) . "\": [\n");
+                  $count = 0;
+                  foreach($values as $value) {
+                    if (!empty($value) && $value != '""') {
+                      $count++;
+                      echo "               {\n";
+                      echo("                    \"@id\": " . $value . "\n");
+
+                      if ($count < count($values)) {
+                        echo "               },\n";
+                      } else {
+                        echo "               }\n";
+                      }
+                    }
+                  }
+                  echo "          ],\n";
+                }
+              }
+
+              foreach($fields as $field) {
+                if (substr($field->name, 0, 4) == "ldp_") {
+                  if (!in_array($field->name, $fieldNotToRender)) {
+                    echo('          "'.substr($field->name, 4).'": ');
+                    echo('' . json_encode(get_post_custom_values($field->name)[0]) . ',');
+                    echo "\n";
+                  }
+                }
               }
             }
 
