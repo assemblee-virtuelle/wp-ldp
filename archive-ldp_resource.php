50cf1e5b3bb8a911f0a98723e65d8ed5c64b2dbf
balessan <benoit.alessandroni@gmail.com>
2016-03-23 18:54:48 +0100

    Adding some information in the container from the resources

--- archive-ldp_resource.php
+++ archive-ldp_resource.php
@@ -3,11 +3,32 @@
 {
     "@context": "<?php echo get_option('ldp_context', 'http://owl.openinitiative.com/oicontext.jsonld'); ?>",
     "@graph": [ {
-        "@id" : "",
+        "@id" : "<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>",
         "@type" : "http://www.w3.org/ns/ldp#BasicContainer",
         "http://www.w3.org/ns/ldp#contains" : [
             <?php while (have_posts()) : the_post(); ?>{
-                "@id": "<?php the_permalink(); ?>"
+                "@id": "<?php the_permalink(); ?>",
+                <?php
+                  $values = get_the_terms($post->ID, 'ldp_container');
+                  if (empty($values[0])) {
+                    $value = reset($values);
+                  } else {
+                    $value = $values[0];
+                  }
+
+                  $termMeta = get_option("ldp_container_$value->term_id");
+                  $modelsDecoded = json_decode($termMeta["ldp_model"]);
+                  $fields = $modelsDecoded->{$value->slug}->fields;
+                  foreach ($fields as $field) {
+                    if (($field->name == "ldp_foaf:firstName"
+                          || $field->name == "ldp_foaf:name")
+                          && !empty(get_post_custom_values($field->name)[0])) {
+                      echo('    "'.substr($field->name, 4).'": ');
+                      echo('' . json_encode(get_post_custom_values($field->name)[0]) . ',');
+                      echo "\n";
+                    }
+                  }
+                ?>
             }<?php if($wp_query->current_post + 1 < $wp_query->post_count) echo(","); ?>
         <?php endwhile; ?>
 ]}
