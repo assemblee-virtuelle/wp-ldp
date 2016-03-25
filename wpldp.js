7d764109f73888e1454ff371eb4e3d7ed833d34e
balessan <benoit.alessandroni@gmail.com>
2016-03-25 16:10:57 +0100

    Automatically refresh the page by submitting the form when changing container

---
+++ wpldp.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+  $('input:radio[name="tax_input[ldp_container][]"]').click(function() {
+      //TODO: Switch to a smooth AJAX call for changing the container, instead of reloading the page.
+      // var value = $(this).val();
+      // console.log('Radio button selected value: ', value);
+      // console.log(store);
+      // var firstLabel = $('#ldp_container-' + value).find('label')[0];
+      // var newContainerName = firstLabel.innerText.toLowerCase();
+      //
+      // console.log(newContainerName);
+      // store.render('#ldpform', containerName, undefined, undefined, newContainerName, 'ldp_');
+
+      var form = $('#post');
+      form.submit();
+  });
+});
