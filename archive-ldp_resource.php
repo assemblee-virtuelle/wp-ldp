4ad788417f074fa58f77769272fc8243dfa6e5d4
balessan <benoit.alessandroni@gmail.com>
2016-03-29 16:17:46 +0200

    Providing fix on containers regarding the commas distribution in hte file

--- archive-ldp_resource.php
+++ archive-ldp_resource.php
@@ -6,9 +6,8 @@
         "@id" : "<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>",
         "@type" : "http://www.w3.org/ns/ldp#BasicContainer",
         "http://www.w3.org/ns/ldp#contains" : [
-            <?php while (have_posts()) : the_post(); ?>{
-                "@id": "<?php the_permalink(); ?>",
-                <?php
+            <?php while (have_posts()) : the_post(); ?>
+<?php
                   $values = get_the_terms($post->ID, 'ldp_container');
                   if (empty($values[0])) {
                     $value = reset($values);
@@ -25,15 +24,16 @@
                   foreach ($fields as $field) {
                     if (in_array($field->name, $includedFieldsList)
                           && !empty(get_post_custom_values($field->name)[0])) {
-                      echo('"'.substr($field->name, 4).'": ');
+                      echo('                "'.substr($field->name, 4).'": ');
                       echo(json_encode(get_post_custom_values($field->name)[0]) . ",\n");
                     }
                   }
 
                   $rdfType = isset($termMeta["ldp_rdf_type"]) ? $termMeta["ldp_rdf_type"] : null;
-                  if (!empty($rdfType)) echo "\"@type\" : \"$rdfType\"\n";
+                  if (!empty($rdfType)) echo "                \"@type\" : \"$rdfType\",\n";
                 ?>
-            }<?php if($wp_query->current_post + 1 < $wp_query->post_count) echo(","); ?>
+                "@id": "<?php the_permalink(); ?>"
+            }<?php if($wp_query->current_post + 1 < $wp_query->post_count) { echo(",\n"); } else { echo("\n"); } ?>
         <?php endwhile; ?>
 ]}
     ]
