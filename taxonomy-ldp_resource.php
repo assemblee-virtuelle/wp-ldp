33fa7354900f160ce405ca01f8a0bc99449f1407
balessan <benoit.alessandroni@gmail.com>
2016-04-02 21:34:31 +0200

    Fixing divergence between 2 templates

--- taxonomy-ldp_resource.php
+++ taxonomy-ldp_resource.php
@@ -3,17 +3,38 @@
 {
     "@context": "<?php echo get_option('ldp_context', 'http://owl.openinitiative.com/oicontext.jsonld'); ?>",
     "@graph": [ {
-        "@id" : "",
+        "@id" : "<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>",
         "@type" : "http://www.w3.org/ns/ldp#BasicContainer",
         "http://www.w3.org/ns/ldp#contains" : [
-          <?php
-          $output = 'objects'; // or names
-          $terms = get_terms(array('ldp_container'), array('hide_empty'=>false));
-          foreach( $terms as $term ) :
-             ?>{
-                "@id": "<?php echo site_url(); ?>/ldp_resource/<?php echo $term->slug;?>"
-            }
-        <?php endforeach; ?>
+            <?php while (have_posts()) : the_post(); ?>{
+<?php
+                  $values = get_the_terms($post->ID, 'ldp_container');
+                  if (empty($values[0])) {
+                    $value = reset($values);
+                  } else {
+                    $value = $values[0];
+                  }
+
+                  $termMeta = get_option("ldp_container_$value->term_id");
+                  $ldpIncludedFieldsList = isset($termMeta['ldp_included_fields_list']) ? $termMeta['ldp_included_fields_list'] : null;
+                  $modelsDecoded = json_decode($termMeta['ldp_model']);
+
+                  $includedFieldsList = !empty($ldpIncludedFieldsList) ? array_map('trim', explode(',', $ldpIncludedFieldsList)) : null;
+                  $fields = $modelsDecoded->{$value->slug}->fields;
+                  foreach ($fields as $field) {
+                    if ((!empty($includedFieldsList) && in_array($field->name, $includedFieldsList))
+                          && !empty(get_post_custom_values($field->name)[0])) {
+                      echo('                "'.substr($field->name, 4).'": ');
+                      echo(json_encode(get_post_custom_values($field->name)[0]) . ",\n");
+                    }
+                  }
+
+                  $rdfType = isset($termMeta["ldp_rdf_type"]) ? $termMeta["ldp_rdf_type"] : null;
+                  if (!empty($rdfType)) echo "                \"@type\" : \"$rdfType\",\n";
+                ?>
+                "@id": "<?php the_permalink(); ?>"
+            }<?php if($wp_query->current_post + 1 < $wp_query->post_count) { echo(",\n"); } else { echo("\n"); } ?>
+        <?php endwhile; ?>
 ]}
     ]
 }
