4e09771f569ebbbcc36fd652a27665866861496a
Benoît Alessandroni <benoit.alessandroni@gmail.com>
2017-07-26 22:43:27 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -68,7 +68,8 @@ cd ../../svn-trunk/
 # Find all .svn dirs in sub dirs
 SVN_DIRS=`find . -type d -iname .svn`
 
-for SVN_DIR in $SVN_DIRS; do
+for SVN_DIR in $SVN_DIRS
+do
     SOURCE_DIR=${SVN_DIR/.}
     TARGET_DIR=$TARGET${SOURCE_DIR/.svn}
     TARGET_SVN_DIR=$TARGET${SVN_DIR/.}
